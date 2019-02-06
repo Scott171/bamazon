926b6d3bbe2b0ce826044cf3ba762068431f8147
Scott171 <K.Scott.Hewitson@gmail.com>
2019-02-05 17:17:16 -0700

    finished customer homework

---
+++ bamazonCustomer.js
@@ -0,0 +1,77 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon_db"
+});
+
+var buyItem = function() {
+    connection.query('SELECT * FROM products', function(err, res) {
+        
+
+        console.log("==============Items=For=Sell===============");
+        console.log("====ID===========Item============Price=====");
+      if (err) throw err;
+   
+      for (var i=0; i<res.length; i++) {
+      
+        console.log(" | "+res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + "");
+       
+      }
+        inquirer.prompt([{
+            name: "itemId",
+            type: "input",
+            message: "What is the item ID you would like to buy?",
+            validate: function(value) {
+                if (isNaN(value) == false) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }, {
+            name: "Quantity",
+            type: "input",
+            message: "How many of this item would you like to buy?",
+            validate: function(value) {
+                if (isNaN(value) == false) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }]).then(function(answer) {
+            var chosenId = answer.itemId - 1
+            var chosenQuantity = answer.Quantity
+            if (chosenQuantity < res[chosenId].stock) {
+                console.log("Your total for " + "(" + answer.Quantity + ")" + " - " + res[chosenId].product_name+ " is: $" + res[chosenId].price * chosenQuantity);
+                connection.query("UPDATE products SET ? WHERE ?", [{
+                    stock: res[chosenId].stock - chosenQuantity
+                }, {
+                    item_id: res[chosenId].item_id
+                }], function(err, res) {
+                    //console.log(err);
+                    buyItem();
+                });
+
+            } else {
+                console.log("Sorry, insufficient stock at this time. All we have is " + res[chosenId].stock + " in stock.");
+                buyItem();
+            }
+        })
+    })
+}
+
+
+buyItem();
