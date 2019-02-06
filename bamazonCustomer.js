0cd37d719d53c43f496e9259e6aaced0d8cea07e
Scott171 <K.Scott.Hewitson@gmail.com>
2019-02-05 17:20:37 -0700

    finished

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,37 +3,36 @@ var inquirer = require("inquirer");
 
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
 
-  // Your username
-  user: "root",
+    port: 3306,
 
-  // Your password
-  password: "",
-  database: "bamazon_db"
+
+    user: "root",
+
+    password: "",
+    database: "bamazon_db"
 });
 
-var buyItem = function() {
-    connection.query('SELECT * FROM products', function(err, res) {
-        
+var buyItem = function () {
+    connection.query('SELECT * FROM products', function (err, res) {
+
 
         console.log("==============Items=For=Sell===============");
         console.log("====ID===========Item============Price=====");
-      if (err) throw err;
-   
-      for (var i=0; i<res.length; i++) {
-      
-        console.log(" | "+res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + "");
-       
-      }
+        if (err) throw err;
+
+        for (var i = 0; i < res.length; i++) {
+
+            console.log(" | " + res[i].item_id + " | " + res[i].product_name + " | " + res[i].price + "");
+
+        }
         inquirer.prompt([{
             name: "itemId",
             type: "input",
             message: "What is the item ID you would like to buy?",
-            validate: function(value) {
+            validate: function (value) {
                 if (isNaN(value) == false) {
                     return true;
                 } else {
@@ -44,24 +43,24 @@ var buyItem = function() {
             name: "Quantity",
             type: "input",
             message: "How many of this item would you like to buy?",
-            validate: function(value) {
+            validate: function (value) {
                 if (isNaN(value) == false) {
                     return true;
                 } else {
                     return false;
                 }
             }
-        }]).then(function(answer) {
+        }]).then(function (answer) {
             var chosenId = answer.itemId - 1
             var chosenQuantity = answer.Quantity
             if (chosenQuantity < res[chosenId].stock) {
-                console.log("Your total for " + "(" + answer.Quantity + ")" + " - " + res[chosenId].product_name+ " is: $" + res[chosenId].price * chosenQuantity);
+                console.log("Your total for " + "(" + answer.Quantity + ")" + " - " + res[chosenId].product_name + " is: $" + res[chosenId].price * chosenQuantity);
                 connection.query("UPDATE products SET ? WHERE ?", [{
                     stock: res[chosenId].stock - chosenQuantity
                 }, {
                     item_id: res[chosenId].item_id
-                }], function(err, res) {
-                    //console.log(err);
+                }], function (err, res) {
+
                     buyItem();
                 });
 
